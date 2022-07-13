7a7f1806c75e195b85c74e943ef51bef10875130
brianartea <bri@expertdesign.cc>
2022-07-13 16:45:41 -0400

    commit

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+
+  reactStrictMode: true,
+  images: {
+    domains: ["lh3.googleusercontent.com", "expertdesign.cc"],
+  },
+};
+
+module.exports = nextConfig;
